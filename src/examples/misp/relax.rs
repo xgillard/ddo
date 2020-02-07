4cdd2486a477d315f884392bf1d844e518efd7db
Xavier Gillard <xavier.gillard@uclouvain.be>
2020-02-07 19:58:02 +0100

    ça s’écrit même plus simplement et c’est plus rapide.

--- src/examples/misp/relax.rs
+++ src/examples/misp/relax.rs
@@ -1,8 +1,7 @@
 use bitset_fixed::BitSet;
 
-use crate::core::abstraction::dp::{Problem, SimpleRelaxation};
-use crate::core::abstraction::mdd::MDD;
-use crate::core::common::Decision;
+use crate::core::abstraction::dp::{Problem, Relaxation};
+use crate::core::abstraction::mdd::{MDD, Node};
 use crate::examples::misp::model::Misp;
 
 pub struct MispRelax<'a> {
@@ -14,7 +13,28 @@ impl <'a> MispRelax<'a> {
         MispRelax{pb}
     }
 }
+impl <'a> Relaxation<BitSet> for MispRelax<'a> {
+    fn merge_nodes(&self, _dd: &dyn MDD<BitSet>, nodes: &[&Node<BitSet>]) -> Node<BitSet> {
+        let mut bs   = BitSet::new(self.pb.nb_vars());
+        let mut max_l= i32::min_value();
+        let mut arc  = None;
 
+        for n in nodes {
+            bs |= &n.state;
+
+            if n.lp_len > max_l {
+                max_l = n.lp_len;
+                arc   = n.lp_arc.clone();
+            }
+        }
+
+        Node::new(bs, max_l, arc, false)
+    }
+    fn estimate_ub(&self, n: &Node<BitSet>) -> i32 {
+        n.lp_len + n.state.count_ones() as i32
+    }
+}
+/*
 impl <'a> SimpleRelaxation<BitSet> for MispRelax<'a> {
     fn merge_states(&self, _dd: &dyn MDD<BitSet>, states: &[&BitSet]) -> BitSet {
         let mut bs = BitSet::new(self.pb.nb_vars());
@@ -30,14 +50,14 @@ impl <'a> SimpleRelaxation<BitSet> for MispRelax<'a> {
 
     fn rough_ub(&self, lp: i32, s: &BitSet) -> i32 {
         lp + s.count_ones() as i32
-        /*
-        let mut res = lp;
-        for x in BitSetIter::new(s.clone()) {
-            if s[x.0] {
-                res += self.pb.graph.weights[x.0];
-            }
-        }
-        res
-        */
+
+        //let mut res = lp;
+        //for x in BitSetIter::new(s.clone()) {
+        //    if s[x.0] {
+        //        res += self.pb.graph.weights[x.0];
+        //    }
+        //}
+        //res
+
     }
-}
+}*/
