363a4eb26c4b73e6c496304bbb4d939260d4f718
Xavier Gillard <xavier.gillard@uclouvain.be>
2020-10-23 17:45:16 +0200

    Comments on how to use the NoDupFrontier and the AgressivelyBoundedMDD

--- examples/minla/main.rs
+++ examples/minla/main.rs
@@ -8,8 +8,11 @@ use structopt::StructOpt;
 use ddo::abstraction::solver::Solver;
 use ddo::implementation::mdd::config::config_builder;
 use ddo::implementation::solver::parallel::ParallelSolver;
-
-use ddo::implementation::mdd::aggressively_bounded::AggressivelyBoundedMDD;
+// Uncomment this line if you intend to use the AggressivelyBoundedMDD
+// use ddo::implementation::mdd::aggressively_bounded::AggressivelyBoundedMDD;
+//
+// You should use the NoDupFrontier whenever you can. It is inexpensive and
+// brings massive perf improvement to the overall problem resolution.
 use ddo::implementation::frontier::NoDupFrontier;
 
 use crate::graph::Graph;
@@ -41,10 +44,26 @@ fn main() {
     let threads = opt.threads.unwrap_or_else(num_cpus::get);
     let problem = if opt.dimacs { read_dimacs(&opt.fname) } else { read_gra(&opt.fname) }.unwrap();
     let relax   = MinlaRelax::new(&problem);
-    let config  = config_builder(&problem, relax).build();
-    let mdd     = AggressivelyBoundedMDD::from(config);
+    // =======================================================================
+    // When it turns out that deriving a lower bound (restricted mdd) is too
+    // expensive with the DeepMDD, you might want to use something more 
+    // aggressive. This is done this way:
+    // -> Create the configuration from the problem description and relaxation
+    // -> Build an aggressively bounded mdd
+    // -> Instanciate the parallel solver with a nodup pq (because this helps
+    //    a lot!)
+    // =======================================================================
+    //let config  = config_builder(&problem, relax).build();
+    //let mdd     = AggressivelyBoundedMDD::from(config);
+    // =======================================================================
+    // Simple and plain method (typically more performant) 
+    // -> Use a DeepMDD with local bounds 
+    // -> And use a NoDupFrontier (this is where the miracle occurs)
+    // =======================================================================
+    let mdd = config_builder(&problem, relax).into_deep();
+    // =======================================================================
     let mut solver  = ParallelSolver::customized(mdd, opt.verbose, threads)
-        .with_frontier(NoDupFrontier::default());
+        .with_frontier(NoDupFrontier::default()); // miracle !
 
     let start = SystemTime::now();
     let opt = solver.maximize().best_value.unwrap_or(isize::min_value());
