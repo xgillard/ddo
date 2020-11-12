55b5103e7a9266a8d02ce093bf837979c4555efd
vcoppe <vianney.coppe@student.uclouvain.be>
2020-11-12 12:42:22 +0100

    MinLA : add .mtx parser and clean arguments

--- examples/minla/main.rs
+++ examples/minla/main.rs
@@ -8,12 +8,8 @@ use structopt::StructOpt;
 use ddo::abstraction::solver::Solver;
 use ddo::implementation::mdd::config::config_builder;
 use ddo::implementation::solver::parallel::ParallelSolver;
-// Uncomment this line if you intend to use the AggressivelyBoundedMDD
-use ddo::implementation::mdd::aggressively_bounded::AggressivelyBoundedMDD;
-//
-// You should use the NoDupFrontier whenever you can. It is inexpensive and
-// brings massive perf improvement to the overall problem resolution.
 use ddo::implementation::frontier::NoDupFrontier;
+use ddo::implementation::heuristics::NbUnassignedWitdh;
 
 use crate::graph::Graph;
 use crate::model::Minla;
@@ -27,42 +23,21 @@ mod graph;
 struct Opt {
     /// Path to the instance (*.gra | *.dimacs)
     fname: String,
-    /// Log the progression
-    #[structopt(name="verbose", short, long, default_value = "1")]
-    verbose: u8,
     /// The number of threads to use (default: number of physical threads on this machine)
     #[structopt(name="threads", short, long)]
-    threads: Option<usize>,
-    /// Use dimacs instead of gra
-    #[structopt(name="dimacs", long)]
-    dimacs: bool
+    threads: Option<usize>
 }
 
 fn main() {
     let opt = Opt::from_args();
 
     let threads = opt.threads.unwrap_or_else(num_cpus::get);
-    let problem = if opt.dimacs { read_dimacs(&opt.fname) } else { read_gra(&opt.fname) }.unwrap();
+    let problem = read_file(&opt.fname).unwrap();
     let relax   = MinlaRelax::new(&problem);
-    // =======================================================================
-    // When it turns out that deriving a lower bound (restricted mdd) is too
-    // expensive with the DeepMDD, you might want to use something more 
-    // aggressive. This is done this way:
-    // -> Create the configuration from the problem description and relaxation
-    // -> Build an aggressively bounded mdd
-    // -> Instanciate the parallel solver with a nodup pq (because this helps
-    //    a lot!)
-    // =======================================================================
-    let config  = config_builder(&problem, relax).build();
-    let mdd     = AggressivelyBoundedMDD::from(config);
-    // =======================================================================
-    // Simple and plain method (typically more performant) 
-    // -> Use a DeepMDD with local bounds 
-    // -> And use a NoDupFrontier (this is where the miracle occurs)
-    // =======================================================================
-    //let mdd = config_builder(&problem, relax).into_deep();
-    // =======================================================================
-    let mut solver  = ParallelSolver::customized(mdd, opt.verbose, threads)
+    let mdd  = config_builder(&problem, relax)
+        .with_max_width(NbUnassignedWitdh)
+        .into_deep();
+    let mut solver  = ParallelSolver::customized(mdd, 2, threads)
         .with_frontier(NoDupFrontier::default()); // miracle !
 
     let start = SystemTime::now();
@@ -70,6 +45,18 @@ fn main() {
     let end = SystemTime::now();
 
     println!("Optimum {} computed in {:?} with {} threads", opt, end.duration_since(start).unwrap(), threads);
+}
+
+fn read_file(fname: &str) -> Result<Minla, io::Error> {
+    if fname.contains("gra") {
+        read_gra(fname)
+    } else if fname.contains("dimacs") {
+        read_dimacs(fname)
+    } else if fname.contains("mtx") {
+        read_mtx(fname)
+    } else {
+        read_gra(fname)
+    }
 }
 
 fn read_gra(fname: &str) -> Result<Minla, io::Error> {
@@ -112,6 +99,35 @@ fn read_dimacs(fname: &str) -> Result<Minla, io::Error> {
     for i in 0..n {
         for j in 0..n {
             g[i][j] = graph[(i,j)];
+        }
+    }
+
+    Ok(Minla::new(g))
+}
+
+fn read_mtx(fname: &str) -> Result<Minla, io::Error> {
+    let file = File::open(fname).expect("File not found.");
+    let buffered = BufReader::new(file);
+    let mut n = 0;
+    let mut g = vec![];
+
+    for line in buffered.lines() {
+        let line = line?;
+
+        if line.starts_with("%") {
+            continue;
+        }
+
+        let x: Vec<f32> = line.trim().split_whitespace().map(|s| s.parse::<f32>().unwrap()).collect();
+
+        if n == 0 {
+            n = x[0] as usize;
+            g = vec![vec![0; n]; n];
+        } else {
+            let i = (x[0] as usize)-1;
+            let j = (x[1] as usize)-1;
+            g[i][j] = 1;
+            g[j][i] = 1;
         }
     }
 
