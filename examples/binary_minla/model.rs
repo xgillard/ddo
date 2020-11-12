811f1f2e7e52d1157eb44e992354c8d74f182a6f
vcoppe <vianney.coppe@student.uclouvain.be>
2020-11-12 13:36:09 +0100

    MinLA : add implementation specific to binary weights

---
+++ examples/binary_minla/model.rs
@@ -0,0 +1,113 @@
+use std::hash::{Hash, Hasher};
+use std::ops::Not;
+
+use bitset_fixed::BitSet;
+
+use ddo::common::{BitSetIter, Decision, Domain, Variable, VarSet};
+use ddo::abstraction::dp::Problem;
+
+#[derive(Debug, Clone)]
+pub struct State {
+    pub free : BitSet,
+    pub cut  : Vec<isize>,
+    pub m    : isize
+}
+impl PartialEq for State {
+    fn eq(&self, other: &Self) -> bool {
+        self.free == other.free
+    }
+}
+impl Eq for State {}
+impl Hash for State {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.free.hash(state);
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Minla {
+    pub g : Vec<Vec<isize>>,
+    pub deg : Vec<isize>,
+    pub m : isize
+}
+impl Minla {
+    pub fn new(g : Vec<Vec<isize>>) -> Minla {
+        let n = g.len();
+        let mut deg = vec![0; n];
+        let mut m = 0;
+        for i in 0..n {
+            for j in 0..n {
+                if i != j {
+                    m += g[i][j];
+                    deg[i] += g[i][j];
+                }
+            }
+        }
+        m /= 2;
+        Minla { g, deg, m }
+    }
+
+    fn no_vertex(&self) -> usize {
+        self.nb_vars()
+    }
+}
+impl Problem<State> for Minla {
+    fn nb_vars(&self) -> usize {
+        self.g.len()
+    }
+
+    fn initial_state(&self) -> State {
+        State {
+            free: BitSet::new(self.nb_vars()).not(),
+            cut: vec![0; self.nb_vars()],
+            m: self.m
+        }
+    }
+
+    fn initial_value(&self) -> isize {
+        0
+    }
+
+    fn domain_of<'a>(&self, state: &'a State, var: Variable) -> Domain<'a> {
+        if var.0 == 0 {
+            Domain::from(0..((self.nb_vars()-1) as isize))
+        } else if state.free.count_ones() == 0 { // relaxed node with empty free vertices intersection
+            Domain::from(vec![self.no_vertex() as isize])
+        } else {
+            Domain::from(&state.free)
+        }
+    }
+
+    fn transition(&self, state: &State, _vars: &VarSet, d: Decision) -> State {
+        let i = d.value as usize;
+
+        let mut result = state.clone();
+
+        if i != self.no_vertex() {
+            result.free.set(i as usize, false);
+            result.cut[i] = 0;
+
+            for j in BitSetIter::new(&result.free) {
+                result.cut[j] += self.g[i][j];
+                result.m -= self.g[i][j];
+            }
+        }
+
+        result
+    }
+
+    fn transition_cost(&self, state: &State, _vars: &VarSet, d: Decision) -> isize {
+        let i = d.value as usize;
+
+        let mut cost = 0;
+        if i != self.no_vertex() {
+            for j in BitSetIter::new(&state.free) {
+                if i != j {
+                    cost += state.cut[j] + self.g[i][j]
+                }
+            }
+        }
+
+        - cost
+    }
+}
---
+++ examples/binary_minla/relax.rs
@@ -0,0 +1,90 @@
+use bitset_fixed::BitSet;
+
+use ddo::abstraction::dp::{Problem, Relaxation};
+use ddo::common::{BitSetIter, Decision};
+
+use crate::model::{Minla, State};
+
+use std::cmp::Reverse;
+
+#[derive(Debug, Clone)]
+pub struct MinlaRelax<'a> {
+    pb: &'a Minla
+}
+
+impl <'a> MinlaRelax<'a> {
+    pub fn new(pb : &'a Minla) -> MinlaRelax<'a> {
+        MinlaRelax{pb}
+    }
+
+    fn edge_lb(&self, n : isize, m : isize) -> isize {
+        let mut edge_lb = 0;
+        let mut edges = m;
+
+        for k in 1..n {
+            if edges <= 0 {
+                break;
+            } else {
+                edge_lb += edges;
+                edges -= n - k;
+            }
+        }
+
+        edge_lb
+    }
+
+    fn degree_lb(&self, vertices : &BitSet, state : &State) -> isize {
+        let mut deg_lb = 0;
+
+        for k in BitSetIter::new(&vertices) {
+            let d = self.pb.deg[k] - state.cut[k];
+            deg_lb += (d * d + 2 * d + d % 1) / 4;
+        }
+
+        deg_lb / 2
+    }
+
+    fn cut_lb(&self, state : &State) -> isize {
+        let mut cuts = state.cut.clone();
+
+        // sort decreasingly
+        cuts.sort_by_key(|&b| Reverse(b));
+
+        // cut weights in optimistic order
+        let mut cut_lb = 0;
+        for (dist, cut) in cuts.into_iter().enumerate() {
+            if cut == 0 {
+                break;
+            }
+            cut_lb += dist as isize * cut;
+        }
+
+        cut_lb
+    }
+
+    fn ub(&self, vertices : &BitSet, state : &State) -> isize {
+        let n = vertices.count_ones() as isize;
+        if n == 0 {
+            0
+        } else {
+            - self.cut_lb(state) - self.edge_lb(n, state.m)
+        }
+    }
+}
+impl <'a> Relaxation<State> for MinlaRelax<'a> {
+    fn merge_states(&self, _states: &mut dyn Iterator<Item=&State>) -> State {
+        State {
+            free: BitSet::new(0),
+            cut: vec![0; self.pb.nb_vars()],
+            m: 0
+        }
+    }
+
+    fn relax_edge(&self, _src: &State, dst: &State, _relaxed: &State, _decision: Decision, cost: isize) -> isize {
+        cost + self.ub(&dst.free, &dst)
+    }
+
+    fn estimate(&self, state: &State) -> isize {
+        self.ub(&state.free, &state)
+    }
+}
