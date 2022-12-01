727f79b46133a722d5f6f7492ff7318dbf33aa2d
vcoppe <vianney.coppe@gmail.com>
2022-12-01 13:23:07 +0100

    fix PSP domain function and RUB

--- ddo/examples/psp/ub_utils.rs
+++ ddo/examples/psp/ub_utils.rs
@@ -17,41 +17,7 @@
 // IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-use ddo::{Problem, Variable, Decision};
 use smallbitset::Set32;
-
-use crate::model::{Psp, IDLE};
-
-/// 
-pub fn wagner_whithin(psp: &Psp) -> Vec<usize> {
-    let mut ww = vec![0; psp.horizon];
-
-    let mut state = psp.initial_state();
-
-    for time in (0..psp.horizon).rev() {
-        let x = state.prev_demands.iter().copied().enumerate()
-            .filter(|(_, x)| *x >= time as isize)
-            .max_by_key(|(i, _)| psp.stocking[*i])
-            .map(|(i, _)| i);
-
-        if let Some(x) = x {
-            for k in ww.iter_mut().take(state.prev_demands[x] as usize).skip(time) {
-                *k += psp.stocking[x];
-            }
-    
-            state = psp.transition(&state, Decision{variable: Variable(time), value: x as isize});
-        } else {
-            state = psp.transition(&state, Decision{variable: Variable(time), value: IDLE});
-        }
-    }
-
-    // cumulative sum
-    for i in 1..psp.horizon {
-        ww[i] += ww[i-1];
-    }
-
-    ww
-}
 
 /// returns the cost the minimum spanning trees for all subset of items
 pub fn all_mst(changeover: &Vec<Vec<usize>>) -> Vec<usize> {
@@ -73,22 +39,22 @@ pub fn mst(members: Set32, changeover: &[Vec<usize>]) -> usize {
     } else {
         let mut covered = Set32::empty();
         let mut total = 0;
-        for (i, a) in members.iter().enumerate() {
+        for a in members.iter() {
             if covered.contains(a) {
                 continue;
             }
             let mut emin = usize::MAX;
             let mut bmin = a;
-            for b in members.iter().skip(i+1) {
-               if covered.contains(b) {
+            for b in members.iter() {
+                if a == b {
                     continue;
-               }
-               let edge = changeover[a as usize][b as usize];
-               let edge = edge.min(changeover[b as usize][a as usize]);
-               if edge < emin {
+                }
+                let edge = changeover[a as usize][b as usize];
+                let edge = edge.min(changeover[b as usize][a as usize]);
+                if edge < emin {
                     emin = edge;
                     bmin = b;
-               }
+                }
             }
             total += emin;
             covered = covered.insert(a);
